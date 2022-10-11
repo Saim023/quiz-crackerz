0e85809f857a50393632041617d0b0b08c588f36
saim-hossain <saimhossain023@gmail.com>
2022-10-12 01:12:13 +0600

    quiz toast  added

--- src/Components/QuizDetails/QuizDetails.js
+++ src/Components/QuizDetails/QuizDetails.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import './QuizDetails.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { EyeIcon } from '@heroicons/react/24/solid';
 
 const QuizDetails = ({ qz }) => {
     const { id, name, question, options, correctAnswer } = qz;
@@ -7,20 +10,34 @@ const QuizDetails = ({ qz }) => {
 
     const handleAnswer = (e) => {
 
-        console.log(e)
+        // console.log(e)
 
 
         if (e === correctAnswer) {
-            alert("Congratulations!! Your answer is correct!!")
+            toast("Congratulations!! Your answer is correct!!")
         }
         else {
-            alert('Sorry!! Your answer is wrong!!')
+            toast('Sorry!! Your answer is wrong!!')
         }
+    }
+
+    const handleCorrectAnswer = () => {
+        toast(`Correct answer is : ${correctAnswer}`)
+        console.log(correctAnswer)
     }
 
     return (
         <div>
-            <h3 className='quiz-question'>{ } {question}</h3>
+            <div className='question-container'>
+                <div>
+                    <h3 className='quiz-question'>{ } {question.slice(3, -4)}</h3>
+                </div>
+                <div>
+                    <EyeIcon className="question-icon" onClick={() => handleCorrectAnswer()} ></EyeIcon>
+
+                </div>
+
+            </div>
             <div className='quiz-options'>
                 {
                     options.map(option => <div>
@@ -28,6 +45,7 @@ const QuizDetails = ({ qz }) => {
                     </div>)
                 }
             </div>
+            <ToastContainer />
         </div>
     );
 };
